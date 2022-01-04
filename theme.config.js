396a46a625abdd0a82cd54d3b6475addd5badf8d
solvencino <harshparamar84@gmail.com>
2022-01-04 16:46:07 +0530

    add favicon

--- theme.config.js
+++ theme.config.js
@@ -23,6 +23,7 @@ export default {
   ),
   head: (
     <>
+      <link rel="icon" href="/favicon.png" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta
         name="description"
